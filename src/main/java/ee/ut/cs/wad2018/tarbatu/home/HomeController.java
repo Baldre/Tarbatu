dfc2e59f42f6bf79684a41a89ad966ea48eb46e9
Baldre <30902973+Baldre@users.noreply.github.com>
2018-09-12 09:38:52 +0300

    some more

--- src/main/java/ee/ut/cs/wad2018/tarbatu/home/HomeController.java
+++ src/main/java/ee/ut/cs/wad2018/tarbatu/home/HomeController.java
@@ -1,8 +1,11 @@
 package ee.ut.cs.wad2018.tarbatu.home;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
@@ -10,5 +13,16 @@ public class HomeController {
     @RequestMapping(path = "/test", method = RequestMethod.GET)
     public String getTestPage(){
         return "home/index";
+    }
+
+    @GetMapping(path = "/test-dynamic")
+    public String getTest2Page(
+            @RequestParam(
+                    value="name-from-url",
+                    required = false,
+                    defaultValue = "World") String name,
+            Model model){
+        model.addAttribute("name-template", name);
+        return "home/index_dynamic";
     }
 }
