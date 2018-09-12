b3f326fa30c4057ed192dfd392a3f4112dbcf7ca
Baldre <30902973+Baldre@users.noreply.github.com>
2018-09-12 09:04:10 +0300

    sec conf

---
+++ src/main/java/ee/ut/cs/wad2018/tarbatu/SecurityConfiguration.java
@@ -0,0 +1,19 @@
+package ee.ut.cs.wad2018.tarbatu;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.cors().and().csrf().disable();
+        http.authorizeRequests().antMatchers("/##").permitAll();
+
+    }
+}
