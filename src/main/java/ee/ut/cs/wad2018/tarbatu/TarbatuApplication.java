6f221e5d65afa6cd65b1a7e97cca83dac7dc6b9c
Baldre <30902973+Baldre@users.noreply.github.com>
2018-09-12 08:22:50 +0300

    Gradle

---
+++ src/main/java/ee/ut/cs/wad2018/tarbatu/TarbatuApplication.java
@@ -0,0 +1,12 @@
+package ee.ut.cs.wad2018.tarbatu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TarbatuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TarbatuApplication.class, args);
+	}
+}
---
+++ src/test/java/ee/ut/cs/wad2018/tarbatu/TarbatuApplicationTests.java
@@ -0,0 +1,16 @@
+package ee.ut.cs.wad2018.tarbatu;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TarbatuApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
