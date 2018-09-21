f4b7dc359c314de259aa632834efce9304426c0c
Baldre <30902973+Baldre@users.noreply.github.com>
2018-09-22 00:14:50 +0300

    Finally got heroku to work

---
+++ src/main/java/ee/ut/cs/wad2018/tarbatu/user/UserRepository.java
@@ -0,0 +1,16 @@
+package ee.ut.cs.wad2018.tarbatu.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    // SELECT * FROM users
+    @Override
+    List<User> findAll();
+    @Query(value = "SELECT * FROM users WHERE userId = :userId",
+            nativeQuery = true)
+    User findByUserId(@Param("userId") Long userId);
+}
