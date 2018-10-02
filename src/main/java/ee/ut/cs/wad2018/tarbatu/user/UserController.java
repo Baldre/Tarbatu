0e269c5d91767e4975a8fbb03ae0145156b9e6dc
Baldre <30902973+Baldre@users.noreply.github.com>
2018-10-02 23:08:25 +0300

    Revert "up?"
    This reverts commit 2412919efbda69e74b80bd249803436f4bc82a33.

--- src/main/java/ee/ut/cs/wad2018/tarbatu/user/UserController.java
+++ src/main/java/ee/ut/cs/wad2018/tarbatu/user/UserController.java
@@ -1,9 +1,10 @@
 package ee.ut.cs.wad2018.tarbatu.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class UserController {
