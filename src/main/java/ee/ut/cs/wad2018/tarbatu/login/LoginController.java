7a6fcb478230ea1983c25abceffdc38d808965b3
kekarind <karl_erik97@hotmail.com>
2018-10-06 19:38:06 +0300

    Login option on index screen

--- src/main/java/ee/ut/cs/wad2018/tarbatu/login/LoginController.java
+++ src/main/java/ee/ut/cs/wad2018/tarbatu/login/LoginController.java
@@ -29,7 +29,7 @@ public class LoginController {
         }
 
         //Kui on valed andmed.
-        model.addAttribute("Valed andmed", true);
+        model.addAttribute("invalidCredentials", true);
         return "login/login";
     }
 }
