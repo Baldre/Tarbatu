28fac90e1de421e5e2bcdb59941de5d4607a9f3a
Baldre <30902973+Baldre@users.noreply.github.com>
2018-10-17 10:29:21 +0300

    Up

--- src/main/java/ee/ut/cs/wad2018/tarbatu/SecurityConfiguration.java
+++ src/main/java/ee/ut/cs/wad2018/tarbatu/SecurityConfiguration.java
@@ -5,15 +5,24 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.UserInfoTokenServices;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.client.OAuth2ClientContext;
 import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.security.oauth2.client.filter.OAuth2ClientAuthenticationProcessingFilter;
 import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+import org.springframework.security.web.authentication.logout.SimpleUrlLogoutSuccessHandler;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @EnableWebSecurity
 @EnableOAuth2Sso
@@ -40,6 +49,11 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         this.resourceServerProperties = resourceServerProperties;
     }
 
+    @Autowired
+    public DefaultTokenServices tokenServices() {
+        return new DefaultTokenServices();
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable();
@@ -55,6 +69,16 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .and()
                 .logout()
                 .logoutSuccessUrl("/index.html");*/
+
+        http.logout().logoutSuccessHandler(new SimpleUrlLogoutSuccessHandler() {
+            @Override
+            public void onLogoutSuccess(HttpServletRequest request,
+                                        HttpServletResponse response,
+                                        Authentication authentication) {
+                OAuth2AccessToken token = tokenServices().getAccessToken((OAuth2Authentication) authentication);
+                tokenServices().revokeToken(token.getValue());
+            }
+        });
     }
 
     private OAuth2ClientAuthenticationProcessingFilter filter(){
