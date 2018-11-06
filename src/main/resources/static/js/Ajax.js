73d7e830284898c975f6917eaf22a4b0767163a2
KJaagup <karljaagup.kask@gmail.com>
2018-11-06 22:12:43 +0200

    AJAX

--- src/main/resources/static/js/Ajax.js
+++ src/main/resources/static/js/Ajax.js
@@ -3,10 +3,11 @@ var btn = document.getElementById("btn");
 
 btn.addEventListener("click", function () {
     var request = new XMLHttpRequest();
-    request.open("GET", "https://learnwebcode.github.io/json-example/animals-1.json");
+    request.open("GET", "https://raw.githubusercontent.com/Baldre/Tarbatu/master/src/main/resources/static/js/loojad.json");
     request.onload = function () {
         var data = JSON.parse(request.responseText);
         renderHTML(data);
+        btn.classList.add("hide-me");
     };
     request.send();
 });
@@ -15,7 +16,7 @@ function renderHTML(info) {
     var htmlString = "";
 
     for(i = 0; i < info.length; i++){
-        htmlString += "<p>" + info[i];
+        htmlString += "<p>" + info[i].name;
     }
 
     ajaxHoidja.insertAdjacentHTML("beforeend", htmlString );
